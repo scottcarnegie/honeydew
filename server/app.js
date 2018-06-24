3d0862130232c9610e82d2436269efb7af5590d4
Scott Carnegie <scottcarnegie@hotmail.com>
2018-06-24 13:52:06 -0700

    Routes added for users. Mongoose configuration modifiied.

--- server/app.js
+++ server/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const httpLogger = require('morgan');
-const mongoose = require('mongoose');
-const { dbAddress } = require('./config/settings');
 const logger = require('./utils/logger');
+const db = require('./utils/db-connection');
 
 // Route imports
 const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const listsRouter = require('./routes/lists');
 
 const app = express();
 
@@ -16,22 +17,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Connect to database
-mongoose.connect(dbAddress)
+db.establishConnection()
   .then(() => {
-    logger.info('Connection established to MongoDb.');
-
     // Route assignments
     app.use('/', indexRouter);
+    app.use('/users', usersRouter);
+    app.use('/lists', listsRouter);
 
     // Catch 404 and forward to error handler
     app.use((req, res) => (res.status(404).send()));
 
     logger.info('Server startup complete.');
   })
-  .catch((err) => {
-    logger.error(`Connection failed to MongoDb: ${err}, ${dbAddress}`);
-    process.exit();
-  });
+  .catch(() => process.exit());
 
 module.exports = app;
