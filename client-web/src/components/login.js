dc015971711982937922ec6acd9f9ea4fa72bb61
Scott Carnegie <scottcarnegie@hotmail.com>
2018-06-24 22:44:13 -0700

    Placeholders to test router.

---
+++ client-web/src/components/login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './login.css';
+
+const login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      
+    </div>
+  );
+}
+
+export default login;
---
+++ client-web/src/components/register.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './register.css';
+
+const register = () => {
+  return (
+    <h1>Register</h1>
+  );
+}
+
+export default register;
